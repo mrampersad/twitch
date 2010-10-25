bbb361a3c9de2e1dc43fc0db7faef80e956f96bc
Martin Rampersad <martin.rampersad@gmail.com>
2010-10-24 23:14:26 -0400

    fixed up config template

--- system/config.template.php
+++ system/config.template.php
@@ -9,27 +9,7 @@ class config
 	
 	public static function prefix()
 	{
-		return '/path/to/application';
-	}
-	
-	public static function db_server()
-	{
-		return '127.0.0.1';
-	}
-	
-	public static function db_username()
-	{
-		return 'username';
-	}
-	
-	public static function db_password()
-	{
-		return 'password';
-	}
-	
-	public static function db_database()
-	{
-		return 'database';
+		return '/url/path';
 	}
 }
 
