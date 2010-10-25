79698b33a99ced815ecb6304e6f1246b9517a012
Martin Rampersad <martin.rampersad@gmail.com>
2010-10-24 23:35:52 -0400

    generic db template

--- application/my_db.template.php
+++ application/my_db.template.php
@@ -5,7 +5,7 @@ class my_db
 	static function open()
 	{
 		$db = new db();
-		$db->connect(config::db_server(), config::db_username(), config::db_password(), config::db_database(), 'utf8');
+		$db->connect('127.0.0.1', 'username', 'password', 'database', 'utf8');
 		return $db;
 	}
 }
