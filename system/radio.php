7009e54678b8f620030859c5c5423d91fc064afc
Martin Rampersad <martin.rampersad@gmail.com>
2010-10-25 20:19:03 -0400

    added radio

---
+++ system/radio.php
@@ -0,0 +1,41 @@
+<?php
+
+class radio
+{
+	protected $name;
+	protected $value;
+	protected $set;
+
+	public function __construct(&$set, $value = '')
+	{
+		$this->name = uuid::gen();
+		$this->value = $value;
+		$this->set = &$set;
+	}
+	
+	public function post()
+	{
+		if(!isset($_POST[$this->name])) throw new Exception('input missing');
+		$this->value = $_POST[$this->name];
+		if(!isset($this->set[$this->value])) throw new Exception('invalid selection');
+	}
+	
+	public function value()
+	{
+		return $this->value;
+	}
+	
+	public function setValue($value)
+	{
+		$this->value = $value;
+	}
+	
+	public function render($value)
+	{
+		echo '<input type="radio"';
+		if($value == $this->value) echo ' checked="checked"';
+		echo ' name="' . util::html($this->name) . '" value="' . util::html($this->value) . '" />';
+	}
+}
+
+?>
