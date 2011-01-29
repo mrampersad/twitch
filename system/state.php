9a0da09726f158de586d931b80c8ac0b1dcf9208
Martin Rampersad <martin.rampersad@gmail.com>
2011-01-28 19:18:09 -0500

    state key instead of huge array

--- system/state.php
+++ system/state.php
@@ -5,32 +5,22 @@ class state
 	public static function save($obj)
 	{
 		$uuid = uuid::gen();
-		$_SESSION['ctrl'][$uuid] = true;
-		file_put_contents(util::base() . '/state/' . $uuid, serialize($obj));
+		file_put_contents(util::base() . '/state/' . $uuid, serialize(array(state::key(), $obj)));
 		return $uuid;
-	}
-	
-	public static function destroy($obj)
-	{
-		foreach(array_keys($_SESSION['ctrl']) as $k)
-		{
-			if($_SESSION['ctrl'][$k] === $obj)
-			{
-				unset($_SESSION['ctrl'][$k]);
-				unlink(util::base() . '/state/' . $k);
-				return;
-			}
-		}
-		
-		throw new exception('invalid object');
 	}
 	
 	public static function load($uuid)
 	{
 		if(!uuid::check($uuid)) throw new Exception('expected uuid');
-		if(!isset($_SESSION['ctrl'][$uuid])) throw new Exception('invalid uuid');
-		$obj = unserialize(file_get_contents(util::base() . '/state/' . $uuid));
+		list($key, $obj) = unserialize(file_get_contents(util::base() . '/state/' . $uuid));
+		if(state::key() !== $key) throw new Exception('permission denied');
 		return $obj;
+	}
+		
+	public static function key()
+	{
+		if(!isset($_SESSION['state_key'])) $_SESSION['state_key'] = uuid::gen();
+		return $_SESSION['state_key'];
 	}
 }
 
